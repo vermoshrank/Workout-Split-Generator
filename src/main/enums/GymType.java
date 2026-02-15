c12ba24a17ad7584495bf0c6fe93ccf685c0702d
George Li <gl8520@students.cs.ubc.ca>
2026-02-14 16:22:51 -0800

    Added GymType Constants

--- src/main/enums/GymType.java
+++ src/main/enums/GymType.java
@@ -1,5 +1,25 @@
 package enums;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import enums.Equipment;
+
 public enum GymType {
+    EVERYTHING_GYM(Equipment.values()),
+    COMMERCIAL_GYM,
+    LOCAL_GYM,
+    HOME_GYM;
+
+    private final ArrayList<Equipment> availableEquipment;
+
+    // EFFECTS: constructs GymType enum with variable number of equipment
+    GymType(Equipment... equipment) {
+        this.availableEquipment = new ArrayList<>(Arrays.asList(equipment));
+    }
+
+    public ArrayList<Equipment> getAvailableEquipment() {
+        return this.availableEquipment;
+    }
 
 }
