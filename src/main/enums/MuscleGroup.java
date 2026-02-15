d7598f1ada96bf6b876ebf62f26f643322f64ac9
George Li <gl8520@students.cs.ubc.ca>
2026-02-14 16:23:10 -0800

    Added MuscleGroup Constants

--- src/main/enums/MuscleGroup.java
+++ src/main/enums/MuscleGroup.java
@@ -1,5 +1,31 @@
 package enums;
 
 public enum MuscleGroup {
+    CHEST,
+    UPPER_CHEST,
 
+    BACK,
+    LATS,
+    ERECTORS,
+    TRAPS,
+
+    SHOULDERS,
+    FRONT_DELTS,
+    SIDE_DELTS,
+    REAR_DELTS,
+
+    ARMS,
+    BICEPS,
+    TRICEPS,
+    FOREARMS,
+
+    LEGS,
+    QUADS,
+    HAMSTRINGS,
+    GLUTES,
+    CALVES,
+
+    CORE,
+    ABS,
+    OBLIQUES
 }
