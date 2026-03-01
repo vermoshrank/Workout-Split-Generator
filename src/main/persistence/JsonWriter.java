2b4b625c3076f85b1f8e4e7dea37263373477088
George <gl8520@students.cs.ubc.ca>
2026-02-28 22:41:32 -0800

    Implemented JsonWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -14,27 +14,32 @@ public class JsonWriter {
 
     // EFFECTS: constructs writer to write to destination file
     public JsonWriter(String destination) {
-        
+        this.destination = destination;
     }
 
     // MODIFIES: this
     // EFFECTS: opens writer and throws FileNotFoundException if destination file cannot
     // be opened for writing
     public void open() throws FileNotFoundException {
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
     // EFFECTS: writes JSON representation of WorkoutPlan to file
     public void write(WorkoutPlan wp) {
+        JSONObject json = wp.toJson();
+        saveToFile(json.toString(TAB));
     }
 
     // MODIFIES: this
     // EFFECTS: closes writer
     public void close() {
+        writer.close();
     }
 
     // MODIFIES: this
     // EFFECTS: writes string to file
     private void saveToFile(String json) {
+        writer.print(json);
     }
 }
