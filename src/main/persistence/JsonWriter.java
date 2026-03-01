164a6c52e3e346bc559f7062522f2403bb2abae2
George <gl8520@students.cs.ubc.ca>
2026-02-28 20:27:12 -0800

    Added Specification for JsonWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,5 +1,40 @@
 package persistence;
 
-public class JsonWriter {
+import org.json.JSONObject;
+import java.io.*;
+import model.WorkoutPlan;
 
+// All based off of WorkRoom app
+
+// Represents a writer that writes JSON representation of workroom to file
+public class JsonWriter {
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
+
+    // EFFECTS: constructs writer to write to destination file
+    public JsonWriter(String destination) {
+        
+    }
+
+    // MODIFIES: this
+    // EFFECTS: opens writer and throws FileNotFoundException if destination file cannot
+    // be opened for writing
+    public void open() throws FileNotFoundException {
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes JSON representation of WorkoutPlan to file
+    public void write(WorkoutPlan wp) {
+    }
+
+    // MODIFIES: this
+    // EFFECTS: closes writer
+    public void close() {
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes string to file
+    private void saveToFile(String json) {
+    }
 }
