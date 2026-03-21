523f5c3bb3ece06f4fbb57d62f272a1a0502c401
George <gl8520@students.cs.ubc.ca>
2026-03-20 18:27:16 -0700

    Added Specifications for RoundedButton

---
+++ src/main/ui/gui/RoundedButton.java
@@ -0,0 +1,18 @@
+package ui.gui;
+
+import java.awt.*;
+import javax.swing.*;
+
+// represents a rounded button that will be used
+public class RoundedButton extends JButton {
+    public RoundedButton(String text) {
+        super(text);
+
+    }
+
+    // Effects: allows for rounded edges
+    @Override
+    protected void paintComponent(Graphics g) {
+
+    }
+}
