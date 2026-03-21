5e48811c49aa350c6b880187bda68fc2335a501a
George <gl8520@students.cs.ubc.ca>
2026-03-20 22:04:49 -0700

    Implemented RoundedPanel

--- src/main/ui/gui/RoundedPanel.java
+++ src/main/ui/gui/RoundedPanel.java
@@ -10,11 +10,16 @@ public class RoundedPanel extends JPanel {
 
     public RoundedPanel(int radius) {
         this.radius = radius;
+        setOpaque(false);
     }
 
     // EFFECTS: allows for rounded edges
     @Override
     protected void paintComponent(Graphics g) {
-
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+        g2.dispose();
     }
 }
