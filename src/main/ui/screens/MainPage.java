ec82c98ef15aa7fc47935b60d66162fe10f31660
George <gl8520@students.cs.ubc.ca>
2026-03-20 22:16:14 -0700

    Added Specifications for MainPage

---
+++ src/main/ui/screens/MainPage.java
@@ -0,0 +1,40 @@
+package ui.screens;
+
+import ui.WorkoutSplitGUI;
+import ui.gui.BaseScreen;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class MainPage extends BaseScreen{
+    public MainPage(WorkoutSplitGUI app) {
+        super(app);
+    }
+
+    // EFFECTS: draws the main content of the page
+    @Override
+    protected JPanel buildContent() {
+        
+    }
+
+    //EFFECTS: draws the "Create New Split" button and "My Saved Splits" label
+    private JPanel buildRow() {
+        
+    }
+
+    // EFFECTS: draws the grid of saved workout splits
+    private JPanel buildSplitsGrid() {
+
+    }
+
+    // EFFECTS: draws an individual workout split card
+    private JPanel buildSplitCard() {
+
+    }
+
+    // EFFECTS: draws card to generate a new workout split
+    private JPanel buildGenerateCard() {
+        
+    }
+
+}
